8034e430f99e5441f098f0233f6aa3cbb39e1b1d
hyper-neutrino <alex.yj.liao@gmail.com>
2022-10-20 16:10:37 -0400

    Create invite.js

---
+++ src/commands/invite.js
@@ -0,0 +1,23 @@
+import { ApplicationCommandType, Colors } from "discord.js";
+import db from "../db.js";
+
+export const command = {
+    type: ApplicationCommandType.ChatInput,
+    name: "bind",
+    description: "obtain a one-time invite",
+    dm_permission: false,
+    default_member_permissions: "0",
+    options: [],
+};
+
+export async function execute(cmd) {
+    await cmd.deferReply({ ephemeral: true });
+
+    const invite = await cmd.guild.invites.create("809970922701979678", {
+        maxUses: 1,
+        unique: true,
+        maxAge: 0,
+    });
+  
+    await cmd.editReply(invite.url);
+}
