e7384335c705bc713db947ae02519d609e0d6ef6
hyper-neutrino <alex.yj.liao@gmail.com>
2023-02-14 10:20:23 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -94,7 +94,7 @@ export const characters = {
 
 const mhy = (name) => `https://upload-os-bbs.mihoyo.com/game_record/genshin/character_icon/UI_AvatarIcon_${name}.png`;
 
-export function getImage(name) {
+export function get_image(name) {
   return ({
     amber: mhy`Ambor`,
     dainsleif: 'https://i.ibb.co/svCrpqD/d87702b9c1ba7231d3e898906d56a0b7-removebg-preview.png',
